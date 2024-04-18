8eae35b36b2b8c04a120cfe8f4b4cb81621da94b
rhenus9911 <jeonghoon9911@naver.com>
2024-04-18 20:39:28 +0900

    add myCalc, setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = "myCalc",
+    version = '0.0.1',
+    description='oss-dev my calculator 0001',
+    author='jeonghoonbae',
+    author_email='jeonghoon9911@naver.com',
+    url = "https://github.com/rhenus9911/calc0001",
+    download_url = "https://github.com/rhenus9911/calc0001/",
+    packages=['mycalc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
